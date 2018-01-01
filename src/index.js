ebf86b0c77d9484090f91ef1cc245dc2afb1022f
Baris Güler <baris.gueler@blacklane.com>
2018-01-01 16:54:16 +0100

    added eslint conf, updated the lib and tests

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
 /**
- * swaps data within specific object keys to each other
- * @param {Object} obj - The object to be updated
- * @param {Array} sourceProps - The first set of keys within the object (obj)
- * @param {Array} targetProps - The second set of keys within the object (obj)
- * @return {Object} updateObj - Updated object with swapped values
+ * Swaps props data within specific object keys to each other
+ * @param object obj - The object to be updated
+ * @param array sourceProps - The first set of keys within the object (obj)
+ * @param array targetProps - The second set of keys within the object (obj)
+ * @return object updateObj - Updated object with swapped values
  */
-
-//  swapProps = (obj, sourceProps, targetProps) => {
-//   sourceProps.map((sourceKey, idx) => {
-//     ;[obj[sourceKey], obj[targetProps[idx]]] = [
-//       obj[targetProps[idx]],
-//       obj[sourceKey]
-//     ]
-//   })
-//   return obj
-// }
-
-// module.exports = {
-//   swapProps
-// }
-
-/**
- * Iterates over an array, if false is returning whilst loops then it immediately returns;
- * @param array - Array to iterate over
- * @param func - Callback function which returns each item
- */
-export function swapProps(obj, sourceProps, targetProps) {
+export default function (obj, sourceProps, targetProps) {
+  const objToBeUpdated = obj;
   sourceProps.map((sourceKey, idx) => {
-    ;[obj[sourceKey], obj[targetProps[idx]]] = [
-      obj[targetProps[idx]],
-      obj[sourceKey]
-    ]
-  })
-  return obj
+    [objToBeUpdated[sourceKey], objToBeUpdated[targetProps[idx]]] = [
+      objToBeUpdated[targetProps[idx]],
+      objToBeUpdated[sourceKey],
+    ];
+  });
+  return objToBeUpdated;
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,23 +1,19 @@
-import * as Index from '../src/';
+import swapProps from '../src/';
 
 describe('#swapProps', () => {
-  beforeEach(() => {
-    jest.spyOn(console, 'log');
-  });
-
-  afterEach(() => {
-    console.log.mockRestore();
-  });
-
   it('should return the object with two swapped props', () => {
-    const expected = {a: 'a', b: 'b'}
-    const actual = Index.swapProps({a: 'b', b: 'a'}, ['a'], ['b']);
-    expect(expected).toEqual(actual)
+    const expected = { a: 'a', b: 'b' };
+    const actual = swapProps({ a: 'b', b: 'a' }, ['a'], ['b']);
+    expect(expected).toEqual(actual);
   });
 
   it('should return the object with more than two swapped props', () => {
-    const expected = {a: 'a', b: 'b', c: 'c', d: 'd'}
-    const actual = Index.swapProps({a: 'b', b: 'a', c: 'd', d: 'c'}, ['a', 'c'], ['b', 'd']);
-    expect(expected).toEqual(actual)
+    const expected = {
+      a: 'a', b: 'b', c: 'c', d: 'd',
+    };
+    const actual = swapProps({
+      a: 'b', b: 'a', c: 'd', d: 'c',
+    }, ['a', 'c'], ['b', 'd']);
+    expect(expected).toEqual(actual);
   });
 });
